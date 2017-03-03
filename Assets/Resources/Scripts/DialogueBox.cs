416de499703671b8c94b7122de01ab95a4580f29
Lauren Weston <lauren.weston@webappuk.com>
2017-03-03 15:46:16 +0000

    Create abililty to click through text from a text file and display three choices

---
+++ Assets/Resources/Scripts/DialogueBox.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour {
+
+	DialogueParser parser;
+
+	public string dialogue;
+	public string choice1Text;
+	public string choice2Text;
+	public string choice3Text;
+	int lineNum;
+
+	public GUIStyle customStyle;
+	public GUIStyle choiceStyle;
+
+	// Use this for initialization
+	void Start () {
+		dialogue = "";
+		choice1Text = "";
+		choice2Text = "";
+		choice3Text = "";
+		parser = GameObject.Find ("DialogueParser").GetComponent<DialogueParser> ();
+		lineNum = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			dialogue = "Select a choice below. " + lineNum;
+			if (lineNum > 0) {
+				choice1Text = parser.GetPhrase (lineNum - 1);
+				choice2Text = parser.GetPhrase (lineNum);
+				choice3Text = parser.GetPhrase (lineNum + 1);
+				lineNum = parser.GetNextID (lineNum);
+			} else {
+				dialogue = "Finito!";
+				choice1Text = "";
+				choice2Text = "";
+				choice3Text = "";
+			}
+
+		}
+	}
+
+	void OnGUI()
+	{
+		dialogue = GUI.TextField (new Rect (70, 300, 1000, 100), dialogue, customStyle);
+		choice1Text = GUI.TextField (new Rect ( 70, 420, 300, 60), choice1Text, choiceStyle);
+		choice2Text = GUI.TextField (new Rect (420, 420, 300, 60), choice2Text, choiceStyle);
+		choice3Text = GUI.TextField (new Rect (770, 420, 300, 60), choice3Text, choiceStyle);
+	}
+}
---
+++ Assets/Resources/Scripts/DialogueParser.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class DialogueParser : MonoBehaviour {
+
+	List<DialogueLine> lines;
+
+	struct DialogueLine {
+		public int ID;
+		public int nextID;
+		public string phrase;
+		public int rating;
+		public List<string> attracts;
+		public List<string> offends;
+
+		public DialogueLine(int i, int ni, string p, int r, List<string> a, List<string> o)
+		{
+			ID = i;
+			nextID = ni;
+			phrase = p;
+			rating = r;
+			attracts = a;
+			offends = o;
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		string file = "phrases.txt";
+
+		lines = new List<DialogueLine>();
+		LoadDialogue(file);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LoadDialogue(string filename){
+		string path = "Assets/Resources/" + filename;
+		string line;
+		StreamReader r = new StreamReader(path);
+
+		using(r)
+		{
+			do
+			{
+				line = r.ReadLine();
+				if(line != null)
+				{
+					//Make a new line to insert to the list of lines
+					string[] lineValues = line.Split(',');
+
+					//Get numbers from the line for ID, nextID, rating
+					int ID, nextID, rating;
+					if( !Int32.TryParse(lineValues[0], out ID) |
+						!Int32.TryParse(lineValues[1], out nextID) |
+						!Int32.TryParse(lineValues[3], out rating))
+						Console.WriteLine("One of the following is not a number\nID:" + lineValues[0] +
+							"\nNextID:" + lineValues[1] + "\nRating:" + lineValues[3]);
+
+
+					//Create the Attracts and offends arrays from a . separated section of the line
+					List<string> lstAttracts = new List<string>();
+					List<string> lstOffends = new List<string>();
+
+					foreach(string attracts in lineValues[4].Split('.'))
+						lstAttracts.Add(attracts);
+					
+					foreach(string offends in lineValues[5].Split('.'))
+						lstOffends.Add(offends);
+
+
+					//Create a new line object for this line of the text file. Add to the list
+					DialogueLine lineEntry = new DialogueLine(ID, nextID, lineValues[2],
+																rating, lstAttracts, lstOffends);
+					lines.Add(lineEntry);
+				}
+			}
+			while(line != null);
+
+			r.Close();
+		}
+	}
+
+	public int GetID(int lineNumber)
+	{
+		if (lineNumber < lines.Count)
+			return lines [lineNumber].ID;
+
+		return -1;
+	}
+
+	public int GetNextID(int lineNumber)
+	{
+		if (lineNumber < lines.Count)
+			return lines [lineNumber].nextID;
+
+		return -1;
+	}
+
+	public string GetPhrase(int lineNumber)
+	{
+		if (lineNumber != -1 & lineNumber < lines.Count)
+			return lines [lineNumber].phrase;
+
+		return "";
+	}
+
+	public int GetRating(int lineNumber)
+	{
+		if (lineNumber < lines.Count)
+			return lines [lineNumber].rating;
+
+		return -1;
+	}
+
+	public List<string> GetAttracts(int lineNumber)
+	{
+		if (lineNumber < lines.Count)
+			return lines [lineNumber].attracts;
+
+		return null;
+	}
+
+	public List<string> GetOffends(int lineNumber)
+	{
+		if (lineNumber < lines.Count)
+			return lines [lineNumber].offends;
+
+		return null;
+	}
+}
